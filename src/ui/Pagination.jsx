caf5cc924ff77c0f454a1d5fc0fe10a74858ffcd
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2025-05-24 20:16:23 +0330

    add some styles for darkmode

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -21,20 +21,20 @@ function Pagination({}) {
       {currentPage > 1 && (
         <button
           onClick={() => handlePageChange(currentPage - 1)}
-          className="px-4 py-2 bg-gray-200 text-gray-700 rounded-md shadow-md hover:bg-gray-300"
+          className="px-4 py-2 bg-gray-200 dark:bg-slate-300 text-gray-700 rounded-md shadow-md hover:bg-gray-300"
         >
           &lt;
         </button>
       )}
 
-      <span className="px-4 py-2 bg-blue-200 text-gray-900 rounded-md shadow-md">
+      <span className="px-4 py-2 bg-blue-200 dark:bg-slate-500 text-gray-900 dark:text-white rounded-md shadow-md">
         Page {currentPage}
       </span>
 
       {currentPage < Math.ceil(totalVehicles / vehiclesPerPage) && (
         <button
           onClick={() => handlePageChange(currentPage + 1)}
-          className="px-4 py-2 bg-gray-200 text-gray-700 rounded-md shadow-md hover:bg-gray-300"
+          className="px-4 py-2 bg-gray-200 dark:bg-slate-300 text-gray-700 rounded-md shadow-md hover:bg-gray-300"
         >
           &gt;
         </button>
